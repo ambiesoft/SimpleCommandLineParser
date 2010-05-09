e22913d8ddd86f974d9677ce9ce91b4c00b7fca9
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2010-05-09 06:01:19 +0000

    start
    git-svn-id: svn://192.168.3.99/SimpleCommandLineParser@2175 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ SimpleCommandLineParser/SimpleCommandLineParser.h
@@ -0,0 +1,62 @@
+// SimpleCommandLineParser.h
+
+#pragma once
+
+using namespace System;
+
+namespace Ambiesoft {
+
+	public ref class SimpleCommandLineParser
+	{
+	private:
+		System::Collections::Hashtable hashes_;
+		System::Collections::Generic::List<String^> mainargs_;
+
+		bool isOptionLeft(String^ s)
+		{
+			return ( s->StartsWith("/") || s->StartsWith("-") );
+		}
+
+	public:
+		SimpleCommandLineParser(array<String^>^ args)
+		{
+			if ( args == nullptr )
+				return;
+
+			for ( int i=0 ; i < args->Length ; ++i )
+			{
+				String^ s = args[i];
+				if ( isOptionLeft(s) )
+				{
+					s = s->TrimStart(gcnew array<Char>{'/','-'});
+					bool determined = false;
+					if ( args->Length > i )
+					{
+						if ( !isOptionLeft(args[i+i]) )
+						{
+							hashes_[s] = args[i+1];
+							++i;
+							determined = true;
+						}
+					}
+
+					if ( !determined )
+						hashes_[s] = true;
+				}
+				else
+				{
+					mainargs_.Add(s);
+				}
+			}
+		}
+
+		property Object^ default[String^]
+		{
+			Object^ get(String^ s)
+			{
+				return hashes_[s];
+			}
+		}
+
+	};
+}
